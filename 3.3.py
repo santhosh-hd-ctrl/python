dc9e39024b492b3d4f98dd9cb8fef6a1d9e7ec37
Santhosh H D <santhoshhd.in@gmail.com>
2026-06-12 21:23:14 +0530

    Update input processing and modify Input2 output
    Modified Input2 output to double the stripped result.

--- 3.3.py
+++ 3.3.py
@@ -4,5 +4,6 @@ Input2 = input("input2: ")
 Input3 = input("input3: ")
 print("Output: ",Input0.upper()) #input in upper case
 print("Output: ",Input1.lower()) #input in lower case
-print("Output: ",Input2.strip()) #removes excess spaces
+print("Output: ",Input2.strip()*2) #removes excess spaces
 print("Output: ",Input3.replace("Awesome", "Wonderful")) #replaces awesome with great
+
